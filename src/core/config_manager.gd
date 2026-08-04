6fe79a67a056378fd8882ef001acec399b11711c
CrazyMiXian <2952057879@qq.com>
2026-08-04 15:25:02 +0800

    Update config_manager.gd
    基本完成config_manager模块的开发
    1.实现从游戏文件夹中读取配置文件，无法读取配置文件时，使用内置的默认配置
    2.实现用get_value函数来读取配置文件数据

--- src/core/config_manager.gd
+++ src/core/config_manager.gd
@@ -1,1 +1,56 @@
 extends Node
+
+var _config : Dictionary = {}
+const CONFIG_PATH := "res://config.json"
+
+func _ready() -> void:
+	initialize()
+
+func initialize() -> void:
+	load_config()
+
+# 加载配置文件
+func load_config() -> void:
+	# 没有配置文件则使用默认配置
+	if not FileAccess.file_exists(CONFIG_PATH):
+		LoggerGlobal.warn("配置文件%s，使用默认配置" % CONFIG_PATH, self.name)
+		_config = _default_config()
+		return
+	
+	# 有配置文件但是无法读取则报错
+	var file := FileAccess.open(CONFIG_PATH, FileAccess.READ)
+	if file == null:
+		LoggerGlobal.error("无法读取配置文件 - %s" % CONFIG_PATH, self.name)
+		_config = _default_config()
+		return
+	
+	var text := file.get_as_text()
+	file.close()
+	
+	var json = JSON.new()
+	var error := json.parse(text)
+	if error != OK:
+		LoggerGlobal.error("配置文件JSON解析失败: %s" % json.get_error_line(), self.name)
+		return
+		
+	_config = json.data
+
+
+# 获取配置文件中的数据
+func get_value(path:String, default = null):
+	var keys := path.split(".")
+	var current = _config
+	for key in keys:
+		if not current is Dictionary or not current.has(key):
+			return default
+		current = current[key]
+	return current
+
+
+# 无法找到配置文件时，使用默认配置
+func _default_config() -> Dictionary:
+	return{
+		"audio": {
+			"master_volume": 1.0
+		}
+	}
