35879240df3e5bc8c7a08d1a7da4d4b4450c9e23
CrazyMiXian <2952057879@qq.com>
2026-08-07 13:55:24 +0800

    Update input_manager_test_1.gd
    对input_manager的测试基本完成

--- src/test/input_manager_test_1.gd
+++ src/test/input_manager_test_1.gd
@@ -8,4 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	InputManager.is_action_just_pressed("up")
+	#InputManager.is_action_just_pressed("up")
+	#if InputManager.is_action_just_released("down"):
+		#print("down")
+	#if InputManager.get_move_vector():
+		#print(InputManager.get_move_vector())
+	if InputManager.get_aim_vector(Vector2(0,0)):
+		print(InputManager.get_aim_vector(Vector2(0,0)))
+		
+	pass
